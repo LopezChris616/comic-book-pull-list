123f66c4df149799377ba2745051c52cb071c8fa
Christian Lopez <lopezchristian1610@gmail.com>
2023-06-25 23:51:46 -0400

    Add table headers

--- src/components/ComicBookPulls.js
+++ src/components/ComicBookPulls.js
@@ -1,22 +1,40 @@
 import React, { useEffect, useState } from "react";
+import ComicBook from "./ComicBook";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 
 function ComicBookPulls() {
     const [comics, setComics] = useState([]);
+    const headers = ["Title", "Type", "Genre", "Price"];
 
     useEffect(() => {
-        fetch(`https://comic-book-pull-list.onrender.com/${window.location.pathname.slice(1)}`)
+        fetch(`https://comic-book-pull-list.onrender.com${window.location.pathname}`)
             .then(res => res.json())
-            .then(comicList => {
-                setComics(comicList)
-            })
+            .then(comicList => setComics(comicList))
     }, [window.location.pathname])
 
-    const title = comics.map(comic => <p key={comic.id}>{comic.title}</p>)
+    console.log(comics);
+
+    
+
+    const tableHeaders = headers.map(header => <TableCell align="right" key={header}>{header}</TableCell>);
 
     return (
         <>
             <h1>{window.location.pathname.slice(1)}</h1>
-            {title}
+            <TableContainer>
+                <Table sx={{minWidth: 700}}>
+                    <TableHead>
+                        <TableRow>
+                            {tableHeaders}
+                        </TableRow>
+                    </TableHead>
+                </Table>
+            </TableContainer>
         </>
     )
 
