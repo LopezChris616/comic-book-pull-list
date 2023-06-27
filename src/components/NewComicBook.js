69eda92f18b9c5f411db1dfdd11509d5b1bd4d94
Christian Lopez <lopezchristian1610@gmail.com>
2023-06-27 19:04:40 -0400

    Add foform inputs to new comic component

--- src/components/NewComicBook.js
+++ src/components/NewComicBook.js
@@ -1,8 +1,38 @@
 import React, { useState } from "react";
+import Typography from "@mui/material/Typography";
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+import Button from '@mui/material/Button';
 
 function NewComicBook() {
+    const inputStyle = {
+        borderColor: "red"
+    };
+
     return (
-        <h1>FORM</h1>
+        <>
+            <Typography variant="h2">FORM</Typography>
+            <form>
+                <FormControl>
+                    <TextField label="Title" variant="outlined" sx={ inputStyle } className="form-input" />
+                    <TextField label="Type" variant="outlined" sx={ inputStyle } select className="form-input">
+                        <MenuItem value="Ongoing">Ongoing</MenuItem>
+                        <MenuItem value="Mini-Series">Mini-Series</MenuItem>
+                        <MenuItem value="Maxi-Series">Maxi-Series</MenuItem>
+                        <MenuItem value="One-Shot">One-Shot</MenuItem>
+                    </TextField>
+                    <TextField label="Genre" variant="outlined" sx={ inputStyle } className="form-input" />
+                    <TextField label="Price" variant="outlined" sx={ inputStyle } className="form-input" />
+                    <TextField label="Frequency" variant="outlined" sx={ inputStyle } className="form-input" select>
+                        <MenuItem value="Monthly">Monthly</MenuItem>
+                        <MenuItem value="Bi-Weekly">Bi-Weekly</MenuItem>
+                        <MenuItem value="Weekly">Weekly</MenuItem>
+                    </TextField>
+                    <Button size="large" variant="outlined">Add</Button>
+                </FormControl>
+            </form>
+        </>
     )
 }
 
