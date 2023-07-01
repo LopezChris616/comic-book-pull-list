af7351494870bf3aad418b969162e43f05908e65
Christian Lopez <lopezchristian1610@gmail.com>
2023-06-30 23:42:21 -0400

    Change home links from Navlink to Link

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Home() {
     return (
         <div className="home-page">
             <h1 style={{ color: "white" }}>C O M I C / B O O K / P U L L  / L I S T</h1>
-            <NavLink to="/marvel" className="comic-links">Marvel Comics</NavLink>
+            <Link to="/marvel" className="comic-links">Marvel Comics</Link>
 
-            <NavLink to="/dc" className="comic-links">DC Comics</NavLink>
+            <Link to="/dc" className="comic-links">DC Comics</Link>
 
-            <NavLink to="/image" className="comic-links">Image Comics</NavLink>
+            <Link to="/image" className="comic-links">Image Comics</Link>
 
-            <NavLink to="/addComic" className="comic-links">Add Comic Book</NavLink>
+            <Link to="/addComic" className="comic-links">Add Comic Book</Link>
         </div>
     )
 }
