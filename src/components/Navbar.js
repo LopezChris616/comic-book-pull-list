a817f5d70a855148c3e4363674649129be61edc1
Christian Lopez <lopezchristian1610@gmail.com>
2023-06-30 23:53:29 -0400

    Add activeStyle to nav-links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,16 +2,20 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function Navbar() {
+  const linkStyle = {
+    borderBottom: "1px solid white"
+  };
+
     return (
         <div className="navbar">
           <div className="nav-home">
             <NavLink exact to="/" className="comic-links">Pull List</NavLink>
           </div>
           <div className="nav-links">
-            <NavLink to="/marvel" className="comic-links">Marvel Comics</NavLink>
-            <NavLink to="/dc" className="comic-links">DC Comics</NavLink>
-            <NavLink to="/image" className="comic-links">Image Comics</NavLink>
-            <NavLink to="/addComic" className="comic-links">Add Comic Book</NavLink>
+            <NavLink to="/marvel" exact activeStyle={ linkStyle } className="comic-links">Marvel Comics</NavLink>
+            <NavLink to="/dc" exact activeStyle={ linkStyle } className="comic-links">DC Comics</NavLink>
+            <NavLink to="/image" exact activeStyle={ linkStyle } className="comic-links">Image Comics</NavLink>
+            <NavLink to="/addComic" exact activeStyle={ linkStyle } className="comic-links">Add Comic Book</NavLink>
           </div>
         </div>
       );
